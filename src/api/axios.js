97c4fba4a1d3e38351244e85e745fe103241c8fe
patrickmac3 <patrickmaceachen9@gmail.com>
2024-04-15 00:06:29 -0400

    updated base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://the-390.onrender.com/registration-keys/condo-registration-key/';
+const baseURL = 'https://the-390.onrender.com/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
