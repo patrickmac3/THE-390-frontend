0e271f81c3e9e0a02f1e00e3eed1e076dd78a967
patrickmac3 <123498910+patrickmac3@users.noreply.github.com>
2024-05-01 22:34:43 -0400

    Delete src directory

--- src/components/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Footer from './Footer';
-import '@testing-library/jest-dom'
-
-test('renders footer text', () => {
-  render(<Footer />);
-  expect(screen.getByText(/Copyright © CondoCare/i)).toBeInTheDocument();
-});
